12cb9f818c47a0f397631c9375da1d2f3d7bfbd2
tazbingor <tazdingo@qq.com>
2017-05-08 20:20:32 +0800

    工作环境区,页脚样式添加完成

--- testXX-page/source/js/main.js
+++ testXX-page/source/js/main.js
@@ -1,6 +1,21 @@
 $(function () {
-    //轮播
+    //banner轮播
+    var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        // slidesPerView: 3,
+        paginationClickable: true,
+        spaceBetween: 30
+    });
 
+    //图集轮播
+    var swiper2 = new Swiper('#albumSlides', {
+        pagination: '.swiper-pagination',
+        slidesPerView: 4,
+        centeredSlides: true,
+        paginationClickable: true,
+        spaceBetween: 0,
+        grabCursor: true
+    });
 
 
 });

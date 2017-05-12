5ea359165b2f778aaa93203ddfaecc2b101d97a1
tazbingor <tazdingo@qq.com>
2017-05-12 19:37:56 +0800

    单页整体布局及完成

---
+++ testXX-page-plus/js/main.js
@@ -0,0 +1,27 @@
+jQuery(document).ready(function ($) {
+    $(".scroll").click(function (event) {
+        event.preventDefault();
+        $('html,body').animate({scrollTop: $(this.hash).offset().top}, 1000);
+    });
+
+    $(document).ready(function () {
+        $('#whatever').hoverGrid();
+    });
+});
+
+
+$(function () {
+    var pull = $('#pull');
+    menu = $('nav ul');
+    menuHeight = menu.height();
+    $(pull).on('click', function (e) {
+        e.preventDefault();
+        menu.slideToggle();
+    });
+    $(window).resize(function () {
+        var w = $(window).width();
+        if (w > 320 && menu.is(':hidden')) {
+            menu.removeAttr('style');
+        }
+    });
+});

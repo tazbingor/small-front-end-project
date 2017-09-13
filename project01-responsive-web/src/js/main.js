5b873d4d5a2414e404c6b8fc97c2303b1aade46c
tazbingo <tazdingo@qq.com>
2017-09-13 09:27:55 +0800

    首页轮播图完成

--- project01-responsive-web/src/js/main.js
+++ project01-responsive-web/src/js/main.js
@@ -1,3 +1,11 @@
 /**
  * Created by TazBingor on 2017/05/13.
  */
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items:1,
+        loop:true,
+        autoplay:true,
+        autoplayTimeout:3000
+    });
+});

a300a6d0902f88e494b8a421efd406a1fb092837
tazbingo <tazdingo@qq.com>
2017-09-14 13:38:03 +0800

    引入picturefill.min.js库

--- project01-responsive-web/src/js/main.js
+++ project01-responsive-web/src/js/main.js
@@ -1,11 +1,12 @@
 /**
  * Created by TazBingor on 2017/05/13.
  */
-$(document).ready(function(){
+$(document).ready(function () {
     $(".owl-carousel").owlCarousel({
-        items:1,
-        loop:true,
-        autoplay:true,
-        autoplayTimeout:3000
+        items: 1,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause: true
     });
 });

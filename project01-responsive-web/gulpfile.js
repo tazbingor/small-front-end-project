af465bd1843170f956983a0ccefe3327aa75e15d
tazbingo <tazdingo@qq.com>
2017-09-18 07:57:06 +0800

    gulp打包完成

--- project01-responsive-web/gulpfile.js
+++ project01-responsive-web/gulpfile.js
@@ -4,21 +4,29 @@
 var gulp = require('gulp');
 var rev = require('gulp-rev');//添加版本号
 var revReplace = require('gulp-rev-replace');//更新index.html下的引用
-var useref = require('gulp-useref');
-var filter = require('gulp-filter');
-var uglify = require('gulp-uglify');
-var csso = require('gulp-csso');
+var useref = require('gulp-useref');//合并css或js文件
+var filter = require('gulp-filter');//筛选和恢复
+var uglify = require('gulp-uglify');//压缩JS文件
+var csso = require('gulp-csso');//压缩CSS文件
 
 gulp.task('default', function () {
-    // console.log("fa ♂ U");
+
     var jsFilter = filter('**/*.js', {restore: true});
     var cssFilter = filter('**/*.css', {restore: true});
     var indexHtmlFilter = filter(['**/*', '!**/index.html'], {restore: true});
 
     return gulp.src('src/index.html')
-        .pipe(useref()).pipe(jsFilter()).pipe(uglify()).pipe(jsFilter.restore)
-        .pipe(cssFilter).pipe(csso()).pipe(cssFilter.restore)
-        .pipe(indexHtmlFilter).pipe(rev()).pipe(indexHtmlFilter.restore)
-        .pipe(revReplace()).pipe(gulp.dest('dist'));
+        .pipe(useref())
+        .pipe(jsFilter)
+        .pipe(uglify())
+        .pipe(jsFilter.restore)
+        .pipe(cssFilter)
+        .pipe(csso())
+        .pipe(cssFilter.restore)
+        .pipe(indexHtmlFilter)
+        .pipe(rev())
+        .pipe(indexHtmlFilter.restore)
+        .pipe(revReplace())
+        .pipe(gulp.dest('dist'));
 
 });

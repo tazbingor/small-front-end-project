20b6a18b276d030e01c1c3f0f5b16b8628b5da16
tazbingo <tazdingo@qq.com>
2017-09-26 13:35:28 +0800

    创建验证模块

--- test08-form-validation/js/main.js
+++ test08-form-validation/js/main.js
@@ -1,3 +1,25 @@
 /**
  * Created by tazbingo on 2017/9/25.
  */
+//原则:只要有input包含data-rule的自定义属性,就必须接受验证
+
+
+var validator = {};
+validator.validate_min = function (val, rule) {
+
+    return true;
+}
+
+validator.validate_maxlength = function (val, rule) {
+
+    return true;
+}
+
+
+//选中所有input中的data-rule
+
+
+//解析每一个input的验证规则
+
+
+//验证
---
+++ test08-form-validation/js/validator.js
@@ -0,0 +1,3 @@
+/**
+ * Created by tazbingo on 2017/9/26.
+ */
